78a16bef5273efa44790ce1119e89ff99b56ed96
robert@tooconnect.nl <g1Thub2016>
2019-02-21 15:52:32 +0100

    Public MAM

--- src/app/modules/mam/mam.component.ts
+++ src/app/modules/mam/mam.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as Mam from '@iota/mam';
 import * as Converter from '@iota/converter';
 
@@ -8,7 +8,8 @@ import * as Converter from '@iota/converter';
   styleUrls: ['./mam.component.scss']
 })
 export class MamComponent implements OnInit {
-mamState
+  mamState;
+
   constructor() {
     this.mamState = Mam.init('https://nodes.devnet.thetangle.org:443');
     this.mamState = Mam.changeMode(this.mamState, 'public');
@@ -16,5 +17,43 @@ mamState
 
   ngOnInit() {
   }
+
+
+  clickPublish() {
+    this.publish('Super public message');
+    this.publish('Super public message2');
+  }
+
+  clickReadMam() {
+    this.readMam();
+  }
+
+  async publish(data) {
+    const trytes = Converter.asciiToTrytes(data);
+    const message = Mam.create(this.mamState, trytes);
+
+    // Update the MAM state to the state of this latest message
+    // We need this so the next publish action knows it's state
+    this.mamState = message.state;
+
+    // Attach the message
+    await Mam.attach(message.payload, message.address, 3, 9);
+    console.log('Sent message to the Tangle!');
+    console.log('Address: ' + message.root);
+  }
+
+
+// Display coordinate data on our screen when we receive it
+  async showData(raw) {
+    const data = Converter.trytesToAscii(raw);
+    console.log(data);
+  }
+
+  async readMam() {
+    // const root = 'OXPOYTSZEOGUOITHYBWDYHXNCLRVQHGXMFMEIJLNHNDEWJYWQHGRPTQJ99MUWRDAOVPBIGSW9MSQLMOOA';
+    const     root ='UHC9NVXVFOPJJMWYJGIDOHYIK9IVIEZXGLKMOQHAZCBQOBKLGXNJW9AQFGK9OIRXFOXQJARNQ9LNQQSQE';
+    await Mam.fetch(root, 'public', null, this.showData);
+  }
+
 
 }
